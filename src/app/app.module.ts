1338de5d0ee1aec9087a46c19d5c0ee0a5310cd6
KattySalinas <katty.salinas@unl.edu.ec>
2020-02-04 16:28:50 -0500

    registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'login', component: LoginComponent},
+    { path: 'registro', component: RegistroComponent},
     { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
